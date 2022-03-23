e0f45d2a4d2bd3c522812911380ceea4c266ea9e
RealIB1 <ibeevpersie@gmail.com>
2022-03-23 11:51:03 +0000

    Initial commit

---
+++ Counter.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+  increment() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  
+}
+  decrement() {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <header className="header">COUNTER MACHINE </header>
+        <div className="count">Counter: {this.state.count}</div>
+        <button onClick={() => this.increment()}>Increase</button>
+        <button onClick={() => this.decrement()}>Decrease</button>
+      </div>
+    );
+  }
+}
+
+export default Hello;
